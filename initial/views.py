8ecf0da0fdafb92a4a843e18d43ebd16f64df508
marianabritoazevedo <mari.brito.azevedo@gmail.com>
2020-10-29 20:48:21 -0300

    Form de contato quase pronto

--- initial/views.py
+++ initial/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.urls import reverse
+from .forms import ContactMessage
 
 # Create your views here.
 def index(request):
 	template_name = 'initial/index.html'
-	return render(request, template_name)
+	context = {}
+	if request.method == 'POST':
+		form = ContactMessage(request.POST)
+		if form.is_valid():
+			context['is_valid'] = True
+			form.send_mail()
+			form = ContactMessage()
+	else:
+		form = ContactMessage()
+	context['form'] = form
+	return render(request, template_name, context)
