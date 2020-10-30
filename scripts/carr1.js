68982735d32c300c6a6da6858df24a4acf43525b
Murilo <murilopaiva22@gmail.com>
2020-10-30 04:44:55 -0300

    01: ADICIONA O PROJETO

---
+++ scripts/carr1.js
@@ -0,0 +1,7 @@
+$('.carr1').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+    });
---
+++ scripts/carr2.js
@@ -0,0 +1,28 @@
+$('.carr2').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    autoplay: true,
+    autoplaySpeed: 500,
+    slidesToShow: 5,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
