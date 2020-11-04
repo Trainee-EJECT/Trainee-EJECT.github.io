051065320f2db0a5100421e81b333aee2b075185
marianabritoazevedo <mari.brito.azevedo@gmail.com>
2020-11-04 10:33:54 -0300

    Envio Final

--- initial/views.py
+++ initial/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.urls import reverse
 from .forms import ContactMessage
+from blog.models import Blog
+from django.utils import timezone
 
 # Create your views here.
 def index(request):
+	blog1 = Blog.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[0]
+	blog2 = Blog.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[1]
+	blog3 = Blog.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[2]
 	template_name = 'initial/index.html'
 	context = {}
 	if request.method == 'POST':
@@ -14,5 +19,11 @@ def index(request):
 			form = ContactMessage()
 	else:
 		form = ContactMessage()
-	context['form'] = form
+	#context['form'] = form
+	context = {
+		'form': form,
+		'blog1': blog1,
+		'blog2': blog2,
+		'blog3': blog3,
+	}
 	return render(request, template_name, context)
